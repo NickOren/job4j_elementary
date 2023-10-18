3d173e8c227e2d39f2ed20a1a3c521a8f44b4840
NickOren <ya@nickoren.ru>
2023-10-18 20:01:13 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nickolay M.");
+        System.out.println("17.05.1977");
+    }
+}
